b1a9080fa9d5a7769cd79ae979d335a71eb0c786
Alex <thindie1@mail.ru>
2022-10-20 21:18:03 +0200

    domaing section done

---
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopListUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.shoppinglist.domain
+
+class GetShopListUseCase {
+    fun getShopList() : List<Shopitem>{
+        TODO()
+    }
+}
---
+++ app/src/main/java/com/example/shoppinglist/domain/ShopItem.kt
@@ -0,0 +1,8 @@
+package com.example.shoppinglist.domain
+
+data class Shopitem(
+    val id : Int,
+    val name: String,
+    val count: Int,
+    val enabled: Boolean
+)
